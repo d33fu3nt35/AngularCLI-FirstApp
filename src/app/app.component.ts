fec4a6e631164216cc910749c30c96df8cdd374b
Daniel <d33fu3nt35@gmail.com>
2017-10-30 09:51:29 -0500

    Following the Angular Documentation tutorials, step four complete, dealing with errors & hiccups when a hero isn't selected upon application start up, added extra functionality to app. now the selected hero's information displays below when selected & css of selected hero box shows.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,30 @@ import {Component} from '@angular/core';
   template: `<h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes">
+    <li *ngFor="let hero of heroes" (click)="onselect(hero)" [class.selected]="hero === selectedHero">
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
+  <div *ngIf="selectedHero">
+  <h2>{{selectedHero.name}} details!</h2>
+  <div><label>id: </label>{{selectedHero.id}}</div>
   <div>
     <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name">
+    <input [(ngModel)]="selectedHero.name" placeholder="name">
+  </div>
   </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
   public heroes = HEROES;
+
+  onselect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
 
 export class Hero {
   id: number;
