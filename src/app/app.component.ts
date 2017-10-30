482a0641116c22eb77e1a50bba7fe30d38dfde24
Daniel <d33fu3nt35@gmail.com>
2017-10-30 09:30:05 -0500

    Following the Angular Documentation tutorials, step three complete, displaying the heroes in a list format and adding CSS to the list itself.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,12 @@ import {Component} from '@angular/core';
   selector: 'app-root',
   /*templateUrl: './app.component.html'*/
   template: `<h1>{{title}}</h1>
+  <h2>My Heroes</h2>
+  <ul class="heroes">
+    <li *ngFor="let hero of heroes">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+    </li>
+  </ul>
   <h2>{{hero.name}} details!</h2>
   <div><label>id: </label>{{hero.id}}</div>
   <div>
@@ -18,9 +24,23 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
+  public heroes = HEROES;
 }
 
 export class Hero {
   id: number;
   name: string;
 }
+
+const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Mr. Narco'},
+  {id: 13, name: 'Mr. Bambasto'},
+  {id: 14, name: 'Mr. Celeritas'},
+  {id: 15, name: 'Mr. Magneta'},
+  {id: 16, name: 'Mr. Rubberman'},
+  {id: 17, name: 'Mr. Dynama'},
+  {id: 18, name: 'Mr. Magma'},
+  {id: 19, name: 'Mr. IQ'},
+  {id: 20, name: 'Mr. Tornado'}
+];
